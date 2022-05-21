ea6cc05c8208b0149f74053abddba94d9ec701c7
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-05-21 14:47:16 -0300

    New: implementação da avaliação dos ataques

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,3 +1,4 @@
+import java.awt.datatransfer.SystemFlavorMap;
 import java.util.Random;
 
 public class Algorithm {
@@ -6,7 +7,7 @@ public class Algorithm {
     private static int[] notes;
 
     public static void initialParams(int sizePopulation) {
-       population = new int[sizePopulation][64];
+        population = new int[sizePopulation][64];
     }
 
     public static void generatePopulation(int sizePopulation) {
@@ -40,11 +41,109 @@ public class Algorithm {
                 }
             }
 
+            int attacks = 0;
             for (int line = 0; line < 8; line++) {
+                for (int column = 0; column < 8; column++) {
+                    int queen = chessboard[line][column];
+
+                    if (queen != 1) continue;
+
+                    // Verificação de ataque na mesma linha
+                    for (int k = 0; k < 8; k++) {
+                        if (column == k) continue;
+                        if (chessboard[line][k] == 1) attacks += 1;
+                    }
+
+                    // Verificação de ataque na mesma coluna
+                    for (int k = 0; k < 8; k++) {
+                        if (line == k) continue;
+                        if (chessboard[k][column] == 1) attacks += 1;
+                    }
+
+                    // Variáveis para validações de ataque nas diagonais
+                    boolean flag;
+                    int lineRef;
+                    int columnRef;
+
+                    // Verificação de ataque na diagonal principal superior
+                    flag = true;
+                    lineRef = line > 0 ? line - 1 : -1;
+                    columnRef = column > 0 ? column - 1 : -1;
+                    while (flag) {
+
+                        if (lineRef < 0 || columnRef < 0) {
+                            flag = false;
+                            break;
+                        }
+
+                        if (chessboard[lineRef][columnRef] == 1) attacks += 1;
+
+                        if (lineRef >= 0) lineRef--;
+                        if (columnRef >= 0) columnRef--;
+                    }
+
+                    // Verificação de ataque na diagonal principal inferior
+                    flag = true;
+                    lineRef = line < 7 ? line + 1 : 8;
+                    columnRef = column < 7 ? column + 1 : 8;
+                    while (flag) {
+
+                        if (lineRef > 7 || columnRef > 7) {
+                            flag = false;
+                            break;
+                        }
+
+                        if (chessboard[lineRef][columnRef] == 1) attacks += 1;
+
+                        if (lineRef <= 7) lineRef++;
+                        if (columnRef <= 7) columnRef++;
+                    }
+
+                    // Verificação de ataque na diagonal secundária superior
+                    flag = true;
+                    lineRef = line > 0 ? line - 1 : -1;
+                    columnRef = column < 7 ? column + 1 : 8;
+                    while (flag) {
+
+                        if (lineRef < 0 || columnRef > 7) {
+                            flag = false;
+                            break;
+                        }
+
+                        if (chessboard[lineRef][columnRef] == 1) attacks += 1;
+
+                        if (lineRef >= 0) lineRef--;
+                        if (columnRef <= 7) columnRef++;
+                    }
+
+                    // Verificação de ataque na diagonal secundária inferior
+                    flag = true;
+                    lineRef = line < 7 ? line + 1 : 8;
+                    columnRef = column > 0 ? column - 1 : -1;
+                    while (flag) {
+
+                        if (lineRef > 7 || columnRef < 0) {
+                            flag = false;
+                            break;
+                        }
+
+                        if (chessboard[lineRef][columnRef] == 1) attacks += 1;
+
+                        if (lineRef <= 7) lineRef++;
+                        if (columnRef >= 0) columnRef--;
+                    }
+                }
+            }
+
+            // Avaliar se existem na mesma coluna
+            // Avaliar se existem na mesma linha
+
+            System.out.println("Attack = " + attacks);
+            for (int line = 0; line < 8; line++) {
+                System.out.print("\n");
                 for (int column = 0; column < 8; column++) {
                     System.out.print(chessboard[line][column] + " ");
                 }
-                System.out.print("\n");
             }
         }
     }
